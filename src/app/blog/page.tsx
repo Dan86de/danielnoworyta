22200f0565098999ffec36989d9202e316be1415
Daniel Noworyta <daniel.noworyta@gmail.com>
2023-10-27 16:40:18 +0200

    Add format script.

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -3,7 +3,7 @@ import { SectionWrapper } from "@/components/section";
 export default function BlogPage() {
 	return (
 		<SectionWrapper className="pt-20">
-			<h1>YouTube Blog Page</h1>
+			<h1>Blog Page</h1>
 		</SectionWrapper>
 	);
 }
