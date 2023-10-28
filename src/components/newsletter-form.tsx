c972ecfb268916be77a986f492664348c347c17f
Daniel Noworyta <daniel.noworyta@gmail.com>
2023-10-28 11:19:01 +0200

    Better styles for newsletter form

--- src/components/newsletter-form.tsx
+++ src/components/newsletter-form.tsx
@@ -44,50 +44,48 @@ export function NewsletterForm() {
 	};
 
 	return (
-		<>
-			<Form {...form}>
-				<form onSubmit={form.handleSubmit(processForm)} className="space-y-2 pt-10">
-					<FormField
-						control={form.control}
-						name="name"
-						render={({ field }) => (
-							<FormItem>
-								<FormLabel>Imię</FormLabel>
-								<FormControl>
-									<Input placeholder="Imię" {...field} />
-								</FormControl>
-								<FormDescription>Wpisz Twoje imię.</FormDescription>
-								<FormMessage />
-							</FormItem>
-						)}
-					/>
-					<FormField
-						control={form.control}
-						name="email"
-						render={({ field }) => (
-							<FormItem>
-								<FormLabel>Email</FormLabel>
-								<FormControl>
-									<Input placeholder="Email" {...field} />
-								</FormControl>
-								<FormDescription>Wpisz Twój email.</FormDescription>
-								<FormMessage />
-							</FormItem>
-						)}
-					/>
-					<div className="pt-2">
-						<Button type="submit" className="w-full ">
-							Dołączam
-						</Button>
-					</div>
-					<p className="pt-1 text-xs">
-						„Zapisując się do newslettera, wyrażasz zgodę na otrzymywanie informacji o nowościach,
-						promocjach, produktach i usługach programistafrontend.pl. Administratorem Twoich danych
-						osobowych będzie <strong>Daniel Noworyta z siedzibą w Bytom 41907, Polska</strong>.
-						Twoje dane będą przetwarzane do celów związanych z wysyłką newslettera”
-					</p>
-				</form>
-			</Form>
-		</>
+		<Form {...form}>
+			<form onSubmit={form.handleSubmit(processForm)} className="space-y-2">
+				<FormField
+					control={form.control}
+					name="name"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Imię</FormLabel>
+							<FormControl>
+								<Input placeholder="Imię" {...field} />
+							</FormControl>
+							<FormDescription>Wpisz Twoje imię.</FormDescription>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<FormField
+					control={form.control}
+					name="email"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Email</FormLabel>
+							<FormControl>
+								<Input placeholder="Email" {...field} />
+							</FormControl>
+							<FormDescription>Wpisz Twój email.</FormDescription>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<div className="pt-2">
+					<Button type="submit" className="w-full ">
+						Dołączam
+					</Button>
+				</div>
+				<p className="pt-1 text-xs">
+					„Zapisując się do newslettera, wyrażasz zgodę na otrzymywanie informacji o nowościach,
+					promocjach, produktach i usługach programistafrontend.pl. Administratorem Twoich danych
+					osobowych będzie <strong>Daniel Noworyta z siedzibą w Bytom 41907, Polska</strong>. Twoje
+					dane będą przetwarzane do celów związanych z wysyłką newslettera”
+				</p>
+			</form>
+		</Form>
 	);
 }
