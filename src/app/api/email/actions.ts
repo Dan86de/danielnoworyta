273c153ef5aebe8c9249a7b273d84e4940c75695
Daniel Noworyta <daniel.noworyta@gmail.com>
2024-04-08 18:13:48 +0200

    Update actions.ts

--- src/app/api/email/actions.ts
+++ src/app/api/email/actions.ts
@@ -1,11 +1,16 @@
 "use server";
 
+import * as process from "process";
+
 import { type EmailPageState } from "@/app/email/page.tsx";
+
+const url =
+	process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://danielnoworyta.pl";
 
 export const handler = async (_prevState: EmailPageState, formData: FormData) => {
 	const markdown = formData.get("markdown") as string;
 	const tldr = formData.get("tldr") as string;
-	const res = await fetch("https://danielnoworyta.pl/api/email", {
+	const res = await fetch(`${url}/api/email`, {
 		method: "POST",
 		body: JSON.stringify({
 			markdown,
